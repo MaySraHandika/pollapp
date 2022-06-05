c72fcb5178fc0cae6481537a91dcfb9efb6b0cc0
MaySraHandika <dikapayau492@gmail.com>
2022-06-05 16:21:35 +0700

    commit

---
+++ app/src/main/java/com/example/pollapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pollapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_cagub1);
+    }
+}
